c3b8e2fa4d8adbba9ab633251c5d9370d245a47c
Juma Allan <allanjuma@gmail.com>
2019-10-16 00:21:18 +0300

    Introducing Constants File

---
+++ app/src/main/java/com/androidstudy/devfest19/utils/Constants.kt
@@ -0,0 +1,16 @@
+package com.androidstudy.devfest19.utils
+
+
+object Constants {
+
+    const val PACKAGE_NAME = "com.androidstudy.devfest19"
+
+    /**
+     * Main Dashboard Modules
+     */
+    const val MAIN_DASHBOARD_CARD_MOVIES = "Movies"
+    const val MAIN_DASHBOARD_CARD_MUSIC = "Music"
+    const val MAIN_DASHBOARD_CARD_NEWS = "News"
+    const val MAIN_DASHBOARD_CARD_WEATHER = "Weather"
+
+}
