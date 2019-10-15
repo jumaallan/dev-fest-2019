6124c72c8ffdea323bc7107b2c062c66340ac492
Juma Allan <allanjuma@gmail.com>
2019-10-16 00:11:21 +0300

    Setting up : Timber, Stetho and LeakCanary

---
+++ app/src/main/java/com/androidstudy/devfest19/DevFest19.kt
@@ -0,0 +1,30 @@
+package com.androidstudy.devfest19
+
+import android.app.Application
+import androidx.annotation.Nullable
+import org.jetbrains.annotations.NotNull
+import timber.log.Timber
+
+class DevFest19 : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initKoin()
+        initTimber()
+    }
+
+    private fun initKoin() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun initTimber() {
+        Timber.plant(object : Timber.DebugTree() {
+            @Nullable
+            override fun createStackElementTag(@NotNull element: StackTraceElement): String? {
+                return super.createStackElementTag(element) + ":" + element.lineNumber
+            }
+        })
+    }
+
+}
