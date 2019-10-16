0a13d7ea25e4d3ab6509613580b10f4e554928c5
Juma Allan <allanjuma@gmail.com>
2019-10-16 16:18:27 +0300

    Reworking Dashboard and Main App

--- movies/src/main/java/com/androidstudy/movies/ui/views/MovieActivity.kt
+++ movies/src/main/java/com/androidstudy/movies/ui/views/MovieActivity.kt
@@ -1,8 +1,13 @@
 package com.androidstudy.movies.ui.views
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.androidstudy.devfest19.ui.views.MainActivity
 import com.androidstudy.movies.R
+import com.androidstudy.devfest19.R as devfest
 
 class MovieActivity : AppCompatActivity() {
 
@@ -10,4 +15,30 @@ class MovieActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        onBackPressed()
+        return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(devfest.menu.home_main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == devfest.id.action_home) {
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+            finish()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
