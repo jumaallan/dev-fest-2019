897c48f325ddfb50b4513f5a92deb089cdb717f6
Juma Allan <allanjuma@gmail.com>
2019-10-17 15:40:31 +0300

    Code updates

--- app/src/main/java/com/androidstudy/devfest19/utils/Constants.kt
+++ app/src/main/java/com/androidstudy/devfest19/utils/Constants.kt
@@ -3,9 +3,10 @@ package com.androidstudy.devfest19.utils
 
 object Constants {
 
-    /**
-     * Main Dashboard Modules
-     */
+    // Base URLs
+    const val MOVIES_BASE_URL = "https://rickandmortyapi.com/api/"
+
+    // Main Dashboard Modules
     const val MAIN_DASHBOARD_CARD_MOVIES = "Movies"
     const val MAIN_DASHBOARD_CARD_MUSIC = "Music"
     const val MAIN_DASHBOARD_CARD_NEWS = "News"
