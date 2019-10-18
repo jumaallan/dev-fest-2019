6dffcfe1edc68e4b68535a6c75bd114e1dd4dc0b
Juma Allan <allanjuma@gmail.com>
2019-10-18 09:11:25 +0300

    Introducing KOIN - DI

---
+++ movies/src/main/java/com/androidstudy/movies/di/Modules.kt
@@ -0,0 +1,48 @@
+package com.androidstudy.movies.di
+
+import com.androidstudy.devfest19.BuildConfig
+import com.androidstudy.movies.utils.Utils
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.core.context.loadKoinModules
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+fun injectFeature() = loadFeature
+
+private val loadFeature by lazy {
+
+    loadKoinModules(
+        retrofit
+    )
+}
+
+val retrofit = module(override = true) {
+
+    single {
+
+        val interceptor = HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG) {
+            interceptor.apply { interceptor.level = HttpLoggingInterceptor.Level.BODY }
+        } else {
+            interceptor.apply { interceptor.level = HttpLoggingInterceptor.Level.NONE }
+        }
+
+        // Not used here --->> Necessary if you need to pass in headers/authorizations
+        val client = OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val request = chain.request().newBuilder()
+                    .addHeader("", "")
+                    .build()
+                chain.proceed(request)
+            }.addInterceptor(interceptor).build()
+
+        Retrofit
+            .Builder()
+            .baseUrl(Utils.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+}
